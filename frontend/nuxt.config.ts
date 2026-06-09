9a84983d106c29181cdbbf3b13cd1947a9314b0e
OneZhanym <karyaka_n@iuca.kg>
2026-06-09 12:47:34 +0600

    feat(frontend): install i18n and configure nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,5 +2,29 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  //Подключение Tailwind CSS для стилей и i18n для языков
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
+
+  //Настройка модуля локализации
+  i18n: {
+    // Стратегия 'prefix_except_default' означает, что основной язык (RU) будет без префикса в URL, 
+    // а для EN и ZH добавятся пути /en и /zh
+    strategy: 'prefix_except_default',
+
+    //Список поддерживаемых языков лендинга
+    locales: [
+      {code: 'ru', iso: 'ru-RU', name: 'Русский'},
+      {code: 'en', iso: 'en-US', name: 'English'},
+      {code: 'zh', iso: 'zh-CN', name: '𡗗文 (Chinese)'}
+    ],
+
+    //Включение определение языка браузера пользователя
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root'
+    }
+  },
+
+  compatibilityDate: '2026-06-09'
 })
